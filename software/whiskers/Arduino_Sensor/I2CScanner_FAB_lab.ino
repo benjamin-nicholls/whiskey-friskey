9daa32ab8ad9bda6a77a5d674867ec114dca3a16
deepakjowel95 <51018258+deepakjowel95@users.noreply.github.com>
2023-10-31 16:14:00 +0000

    Update I2CScanner_FAB_lab.ino

--- software/whiskers/Arduino_Sensor/I2CScanner_FAB_lab.ino
+++ software/whiskers/Arduino_Sensor/I2CScanner_FAB_lab.ino
@@ -4,8 +4,13 @@ Copyright (c) 2018 Luis Llamas
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
  ****************************************************/
+
+/***This is the "I2C Address Scanner" code. This code captures the serial address of the sensor connected at the periphiral
+of the Arduino Uno R3. Please be informed that the SDA_PIN and SCL_PIN have been defined in accordance with the Arduino Uno R3
+schematic diagram. If in the future some other MCu is to be used then, the respectives pins have to define with respect to
+the schematics of that specific MCU***/
  
- #include "I2CScanner.h"
+#include "I2CScanner.h"
 #include <Wire.h>
 
 I2CScanner scanner;
